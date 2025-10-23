bde6357b39ba01e8a7c4e8f3752c068409d6bae9
秦芹 <qinqin@qinqins-MacBook-Pro.local>
2025-10-23 15:44:28 +0200

    Add SQL query and output: state yearly average rent data (2015-2025)

--- 02_zillow_rent_index_analysis/us_rent_index_queries.sql
+++ 02_zillow_rent_index_analysis/us_rent_index_queries.sql
@@ -1,2 +1,24 @@
 -- SQL queries for Zillow Rent Index Analysis
--- [To be added]
+
+CREATE TABLE state_year_avg_rent AS
+WITH unpivoted AS (
+    SELECT
+        statename AS state,
+        LEFT(key, 4)::INT AS year,
+        value::NUMERIC AS rent_value
+    FROM us_rent_index
+    CROSS JOIN LATERAL jsonb_each_text(
+        to_jsonb(us_rent_index)
+        - 'regionid' - 'sizerank' - 'regionname' - 'regiontype' - 'statename'
+    )
+    WHERE key ~ '^[0-9]{4}_[0-9]{2}_[0-9]{2}$'
+      AND value IS NOT NULL
+)
+SELECT
+    state,
+    year,
+    ROUND(AVG(rent_value), 2) AS yearly_avg_rent
+FROM unpivoted
+WHERE state IS NOT NULL
+GROUP BY state, year
+ORDER BY state, year;
